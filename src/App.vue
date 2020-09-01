d55ea5e9f0181ec0dc5a0c1afee89c75efa6be34
Marlon Fernandez <marlonfsolis@outlook.com>
2020-09-01 19:36:17 -0400

    First exercise. Fade transition.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-md-offset-3">
+        <h2>Learning animation with Vue.js</h2>
+      </div>
+    </div>
+
+    <hr>
+
+    <!-- First exercise. Creating fade transition. -->
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-md-offset-3">
+        <h4>First exercise</h4>
+        <button class="btn btn-success" @click="showHide">Show/Hide</button>
+        <br><br>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="visible">This is some info</div>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      visible: false
+    }
+  },
+  components: {},
+  methods: {
+    showHide() {
+      this.visible = !this.visible;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*
+* Fade transition.
+* Any Vue transition with name fade will use this classes.
+* But remember if the style tag is marked scoped the classes in this file will not be global.
+*/
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-leave {
+  /*opacity: 1;*/
+}
+
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
 }
 </style>
